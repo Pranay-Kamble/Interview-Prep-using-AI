3b7563b9dfe262d04b99225c67437209335f4e59
Pranay Kamble <iampranaykamble1@gmail.com>
2025-07-27 19:03:52 +0530

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,6 +10,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { FormField } from "@/components/FormField";
 import { useRouter } from "next/navigation";
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import {auth} from "@/firebase/client"
+import {signUp, signIn} from "@/lib/actions/auth.action";
+
 
 const authFormSchema = (type: FormType) => {
 
@@ -36,13 +40,43 @@ const AuthForm = ({type} : {type: FormType}) => {
         }
     })
 
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === 'sign-in') {
+
+                const { email, password } = values
+                const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+                const idToken = userCredential.user.getIdToken()
+
+                if (!idToken) {
+                    toast.error("Sign in failed")
+                    return
+                }
+
+                await signIn({email, idToken})
                 toast.success("Sign in successful.")
                 console.log(`Sign In: ${values}`)
                 router.push('/') //update when other routes are created, for now I am pushing to home page
+
             }else {
+
+                const {name, email, password} = values;
+                const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+
+
+                const result = await signUp({
+                    uid : userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password
+                });
+
+                if (!result?.success) {
+                    toast.error(result?.message)
+                    return;
+                }
+
                 toast.success("Account created. Please login")
                 console.log(`Sign Up: ${values}`)
                 router.push('/sign-in')
